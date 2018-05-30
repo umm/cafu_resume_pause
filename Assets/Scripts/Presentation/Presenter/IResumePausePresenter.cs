396d7cda95698b310d00e6801d231bcc1bb06f7c
mattak <mattak.me@gmail.com>
2018-05-30 13:05:21 +0900

    :seedling: Define IResumePausePresenter

---
+++ Assets/Scripts/Presentation/Presenter/IResumePausePresenter.cs
@@ -0,0 +1,36 @@
+﻿using CAFU.Core.Presentation.Presenter;
+
+namespace CAFU.ResumePause.Presentation.Presenter
+{
+    public interface IResumePausePresenter : IPresenter
+    {
+        void Resume();
+
+        void Pause();
+
+        bool IsPlaying { get; }
+
+        UniRx.IObservable<bool> IsPlayingAsObservable { get; }
+    }
+
+    public static class IResumePausePresenterExtension
+    {
+        /// <summary>
+        /// Switch Pause or Resume.
+        /// </summary>
+        /// <returns>true if playing</returns>
+        public static bool ResumeOrPause(this IResumePausePresenter presenter)
+        {
+            if (presenter.IsPlaying)
+            {
+                presenter.Pause();
+            }
+            else
+            {
+                presenter.Resume();
+            }
+
+            return presenter.IsPlaying;
+        }
+    }
+}
